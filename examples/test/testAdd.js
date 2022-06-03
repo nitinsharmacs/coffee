30bd8e40bc6d90434d5268c6f3649d35425386bc
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2022-06-03 18:05:24 +0530

    Correct examples

--- examples/test/testAdd.js
+++ examples/test/testAdd.js
@@ -2,11 +2,13 @@ const { strictEqual } = require('assert');
 const { add } = require('../src/add.js');
 
 suite(
-  'add',
-  makeTest('should add two numbers', () => {
-    strictEqual(add(2, 3), 5);
-  }),
-  makeTest('should add a positve and negative numbers', () => {
-    strictEqual(add(2, -3), -1);
-  })
+  'greet',
+  () => {
+    it('should greet person', () => {
+      strictEqual('hello', 'hello');
+    });
+    it('should greet person 2', () => {
+      strictEqual('hello', 'hello');
+    });
+  }
 );
