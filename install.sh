621fcb454ce693e98eb6ca335e5d61357dec3e1c
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2022-06-03 18:02:36 +0530

    Working for -w flag

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 
-COFFEE_DIR=~/.coffee
+COFFEE=~/.coffee
 COFFEE_REPORTERS=~/.coffee/reporters
 COFFEE_REPORTER=$COFFEE_REPORTERS/reporter.js
-COFFEE_UTILS=$COFFEE_DIR/utils
-COFFEE=~/bin/coffee
+COFFEE_UTILS=$COFFEE/utils
+COFFEE_EXEC_FILE=~/bin/coffee
 
 COFFEE_REPORTERS_RELATIVE="../.coffee/reporters"
 
 function create_coffee_setup() {
   mkdir -p $COFFEE_REPORTERS
+  mkdir -p $COFFEE_UTILS
 }
 
 function store_reporter() {
@@ -22,25 +23,26 @@ function store_reporter() {
 function store_coffee() {
   local coffee_location=$1
   echo "Downloading Coffee file..."
-  curl -# -o $COFFEE "$coffee_location"
+  curl -# -o $COFFEE_EXEC_FILE "$coffee_location"
   echo "Placed coffee in user bin successfully"
 }
 
 function store_parser() {
-  local parsers_location=$@
+  local parsers_location=(${@})
   local file_locations=("$COFFEE_UTILS/argsParser.js" "$COFFEE_UTILS/createParser.js" "$COFFEE_UTILS/optionParsers.js")
 
   echo "Downloading parsers..."
   local index=0
-  while [[ $index -le ${#parsers_location[@]} ]]; do
-    curl -# -o $COFFEE "${parsers_location[$index]}"
+  while [[ $index -lt ${#parsers_location[@]} ]]
+  do
+    curl -# -o "${file_locations[$index]}" "${parsers_location[$index]}"
     index=$(( index + 1 ))
   done
   echo "Placed parsers successfully..."
 }
 
 function make_coffee_exec() {
-  chmod +x $COFFEE
+  chmod +x $COFFEE_EXEC_FILE
 }
 
 function coffee_path_export() {
@@ -61,7 +63,7 @@ COFFEE_URL="https://raw.githubusercontent.com/nitinsharmacs/coffee/main/src/coff
 PARSERS=("https://raw.githubusercontent.com/nitinsharmacs/coffee/main/src/utils/argsParser.js" "https://raw.githubusercontent.com/nitinsharmacs/coffee/main/src/utils/createParser.js" "https://raw.githubusercontent.com/nitinsharmacs/coffee/main/src/utils/optionParsers.js")
 
 function main() {
-  if [[ ! -e $COFFEE_REPORTERS ]]; then
+  if [[ ! -e $COFFEE_REPORTERS || ! -e $COFFEE_UTILS ]]; then
     create_coffee_setup
   fi
   store_reporter "$REPORTER_URL"
