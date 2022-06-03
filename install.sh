8a09c8f3ba00ebf8e517930605c3758799bd4a75
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2022-06-03 17:46:18 +0530

    Done Installation for -w flag

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
+#! /bin/bash
 
+COFFEE_DIR=~/.coffee
 COFFEE_REPORTERS=~/.coffee/reporters
 COFFEE_REPORTER=$COFFEE_REPORTERS/reporter.js
+COFFEE_UTILS=$COFFEE_DIR/utils
 COFFEE=~/bin/coffee
 
 COFFEE_REPORTERS_RELATIVE="../.coffee/reporters"
@@ -23,38 +26,54 @@ function store_coffee() {
   echo "Placed coffee in user bin successfully"
 }
 
+function store_parser() {
+  local parsers_location=$@
+  local file_locations=("$COFFEE_UTILS/argsParser.js" "$COFFEE_UTILS/createParser.js" "$COFFEE_UTILS/optionParsers.js")
+
+  echo "Downloading parsers..."
+  local index=0
+  while [[ $index -le ${#parsers_location[@]} ]]; then
+    curl -# -o $COFFEE "${parsers_location[$index]}"
+    index=$(( index + 1 ))
+  done
+  echo "Placed parsers successfully..."
+}
+
 function make_coffee_exec() {
   chmod +x $COFFEE
 }
 
-function reporter_path_export() {
-  echo -e "\nexport COFFEE_REPORTERS=\"$COFFEE_REPORTERS_RELATIVE\""
+function coffee_path_export() {
+  echo -e "\nexport COFFEE=\"$COFFEE\""
 }
 
-function set_reporter_path() {
+function set_coffee_path() {
   if [[ -f ~/.bashrc ]]; then
-    reporter_path_export >> ~/.bashrc
+    coffee_path_export >> ~/.bashrc
   fi
   if [[ -f ~/.zshrc ]]; then
-    reporter_path_export >> ~/.zshrc
+    coffee_path_export >> ~/.zshrc
   fi
 }
 
 REPORTER_URL="https://raw.githubusercontent.com/nitinsharmacs/coffee/main/src/reporters/reporter.js"
 COFFEE_URL="https://raw.githubusercontent.com/nitinsharmacs/coffee/main/src/coffee.js"
+PARSERS=("https://raw.githubusercontent.com/nitinsharmacs/coffee/main/src/utils/argsParser.js" "https://raw.githubusercontent.com/nitinsharmacs/coffee/main/src/utils/createParser.js" "https://raw.githubusercontent.com/nitinsharmacs/coffee/main/src/utils/optionParsers.js")
 
 function main() {
   if [[ ! -e $COFFEE_REPORTERS ]]; then
     create_coffee_setup
   fi
   store_reporter "$REPORTER_URL"
+  store_parser "${PARSERS[@]}"
   store_coffee "$COFFEE_URL"
 
   echo "Making coffee executable"
   make_coffee_exec
 
-  echo "Setting up the reporter path"
-  set_reporter_path
+  echo "Setting up the coffee path"
+  set_coffee_path
+
   echo "Installed successfully"
   echo "Happy testing :)"
 }
